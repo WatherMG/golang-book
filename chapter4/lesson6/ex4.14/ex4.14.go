20cfdda0a23948f34297331ca3aa4faa931af2ec
Alex Sysoev <dr.wather@gmail.com>
2023-03-06 02:31:00 +0300

    complete ex4.14 (support issues, issue, milestones, users)

--- chapter4/lesson6/ex4.14/ex4.14.go
+++ chapter4/lesson6/ex4.14/ex4.14.go
@@ -1,1 +1,159 @@
+/*
+Exercise 4.14
+
+Создайте веб-сервер, однократно запрашивающий информацию у GitHub, а затем позволяющий выполнять навигацию
+по списку сообщений об ошибках, контрольных точек и пользователей.
+
+Create a web server that queries GitHub once and then allows navigation of the list of bug reports, milestones, and users.
+*/
+
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"GolangBook/chapter4/lesson6/ex4.14/issues"
+)
+
+var p = "chapter4/lesson6/ex4.14/templates"
+var issueListTemplate = template.Must(template.New("issueList.html").Funcs(
+	template.FuncMap{"formatDate": formatDate}).ParseFiles(p + "/issueList.html"))
+var issueTemplate = template.Must(template.ParseFiles(p + "/issue.html"))
+var milestoneTemplate = template.Must(template.ParseFiles(p + "/milestone.html"))
+var userTemplate = template.Must(template.ParseFiles(p + "/user.html"))
+
+var cache *issues.Cache
+var searchQuery string
+
+// run in browser type localhost:8000/?q=repo:golang/go%20is:open%20json%20decoder
+// or localhost:8000/?q=SEARCH_QUERY
+// To prevent html templates from generating an error, you need to check the path (var p)
+func main() {
+	http.HandleFunc("/", getIssuesList)
+	http.HandleFunc("/issue/", getIssue)
+	http.HandleFunc("/milestone/", getMilestone)
+	http.HandleFunc("/user/", getUser)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func getIssuesList(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query().Get("q")
+
+	if q != "" {
+		if cache.Issues == nil || searchQuery != q { // for reuse cache in same query when refresh
+			searchQuery = q
+			res, err := issues.SearchIssues(q)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			cache = &issues.Cache{
+				IssuesByID:     make(map[int]*issues.Issue, res.TotalCount),
+				MilestonesByID: make(map[int]*issues.Milestone),
+				Users:          make(map[string]*issues.User),
+			}
+
+			cache.Issues = res.Items
+			for _, issue := range res.Items {
+				cache.IssuesByID[issue.Number] = issue
+				if issue.User != nil {
+					u, err := issue.User.GetUser()
+					if err != nil {
+						log.Fatal(err)
+					}
+					cache.Users[issue.User.Login] = u
+				}
+				if issue.Milestone != nil {
+					cache.MilestonesByID[issue.Milestone.Number] = issue.Milestone
+				}
+			}
+		}
+
+		if err := issueListTemplate.Execute(w, cache); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getIssue(w http.ResponseWriter, r *http.Request) {
+	num := getID(w, r)
+	issue, ok := cache.IssuesByID[num]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		_, err := w.Write([]byte(fmt.Sprintf("Issue not found: %d", num)))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := issueTemplate.Execute(w, issue); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getMilestone(w http.ResponseWriter, r *http.Request) {
+	num := getID(w, r)
+	issue, ok := cache.MilestonesByID[num]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		_, err := w.Write([]byte(fmt.Sprintf("Issue not found: %d", num)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if err := milestoneTemplate.Execute(w, issue); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	pathParts := strings.SplitN(r.URL.Path, "/", -1)
+	name := pathParts[2]
+	if err := len(name); err == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		_, err := w.Write([]byte(fmt.Sprintf("User %s not found", name)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	user, ok := cache.Users[name]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		_, err := w.Write([]byte(fmt.Sprintf("User %s not found", user)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if err := userTemplate.Execute(w, user); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getID(w http.ResponseWriter, r *http.Request) int {
+	pathParts := strings.SplitN(r.URL.Path, "/", -1)
+	numStr := pathParts[2]
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := w.Write([]byte(fmt.Sprintf("Issue number isn't a number %s", numStr)))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return num
+}
+
+func formatDate(t time.Time) string {
+	f := t.Format("02.01.2006 | ")
+	return f + strconv.Itoa(int(time.Since(t).Hours()/24)) + "ago"
+}
---
+++ chapter4/lesson6/ex4.14/issues/issues.go
@@ -0,0 +1,129 @@
+/*
+Exercise 4.14
+
+Создайте веб-сервер, однократно запрашивающий информацию у GitHub, а затем позволяющий выполнять навигацию
+по списку сообщений об ошибках, контрольных точек и пользователей.
+
+Create a web server that queries GitHub once and then allows navigation of the list of bug reports, milestones, and users.
+*/
+
+package issues
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+const URL = "https://api.github.com/search/issues"
+const userURL = "https://api.github.com/users/"
+
+var githubToken = os.Getenv("GITHUB_TOKEN")
+
+type IssueSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	Body      string
+	User      *User
+	Milestone *Milestone
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type User struct {
+	PublicRepos int    `json:"public_repos,omitempty"`
+	Followers   int    `json:"followers,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Login       string `json:"login,omitempty"`
+	Type        string `json:"type,omitempty"`
+	Location    string `json:"location,omitempty"`
+	Email       string `json:"email,omitempty"`
+	BIO         string `json:"bio,omitempty"`
+	HTMLURL     string `json:"html_url"`
+}
+
+type Milestone struct {
+	Number      int
+	Title       string
+	Description string
+}
+
+type Cache struct {
+	Issues         []*Issue
+	Users          map[string]*User
+	IssuesByID     map[int]*Issue
+	MilestonesByID map[int]*Milestone
+}
+
+// SearchIssues запрашивает issues у Github
+func SearchIssues(terms string) (*IssueSearchResult, error) {
+	q := url.QueryEscape(terms)
+	resp, err := http.Get(URL + "?q=" + q)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	// Необходимо закрыть resp.Body на всех путях выполнения
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("cбой запроса: %s", resp.Status)
+	}
+	var result IssueSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+
+}
+
+func (u *User) GetUser() (*User, error) {
+	q := url.QueryEscape(u.Login)
+	uri := fmt.Sprintf("%s%s", userURL, q)
+
+	client := &http.Client{}
+
+	buf := &bytes.Buffer{}
+
+	req, err := http.NewRequest(http.MethodGet, uri, buf)
+	req.Header.Set("Authorization", "token "+githubToken)
+	req.Header.Set("Content-Type", "application/vnd.github+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("cбой запроса: %s у пользователя %s", resp.Status, u.Login)
+	}
+	var result User
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (p *Issue) IssueURL() string {
+	return fmt.Sprintf("/issue/%d", p.Number)
+}
+func (p *Issue) MilestoneURL() string {
+	return fmt.Sprintf("/milestone/%d", p.Milestone.Number)
+}
+func (u *User) UserURL() string {
+	return fmt.Sprintf("/user/%s", u.Login)
+}
