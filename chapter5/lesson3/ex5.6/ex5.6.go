465f020a4fd9511bc91d986d85642356edba9d25
Alex Sysoev <dr.wather@gmail.com>
2023-03-09 19:14:30 +0300

    complete ex5.6

--- chapter5/lesson3/ex5.6/ex5.6.go
+++ chapter5/lesson3/ex5.6/ex5.6.go
@@ -1,5 +1,62 @@
+/*
+Exercise 5.6
+Модифицируйте функцию corner из gopl.io/ch3/surface (раздел 3.2), чтобы она использовала именованные
+результаты и инструкцию пустого возврата.
+
+Проблемы:
+Столкнулся с проблемой передачи счетчиков в вызывающую функцию из-за того, что при рекурсии счетчик сбрасывался.
+Решил с помощью указателей, но показалось, что такой код слишком сложен для чтения. Переписал без использования указателей.
+*/
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+const (
+	width, height = 600, 320            // Размер канвы в пикселях
+	cells         = 100                 // Количество ячеек сетки
+	xyrange       = 30                  // Диапазон осей (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // Пикселей в единице x или y
+	zscale        = height * 0.4        // Пикселей в единице z
+	angle         = math.Pi / 6         // Углы осей x, y (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg'> "+
+		"style='stroke: gray; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (sx, sy float64) {
+	// Ищем угловую точку (x,y) ячейки (i,j)
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	// Вычисляем высоту поверхности z
+	z := f(x, y)
+	// Изометрически проецируем (x, y, z) на двумерную канву SVG (sx, sy)
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+	return
+
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // Расстояние от (0,0)
+	return math.Sin(r) / r
 }
