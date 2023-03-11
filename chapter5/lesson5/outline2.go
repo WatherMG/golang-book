cfef22a40506393123ca528b852c67e873c338af
Alex Sysoev <dr.wather@gmail.com>
2023-03-11 14:20:57 +0300

    add notes and examples for lesson 5

---
+++ chapter5/lesson5/outline2.go
@@ -0,0 +1,62 @@
+/*
+Example 5.6
+forEachNode вызывает функции pre(x) и post(x) для каждого узла х в дереве с корнем п.
+Обе функции необязательны.
+рге вызывается до посещения дочерних узлов, a post - после.
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+var depth int
+
+func main() {
+	_ = outline("https://vk.com")
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+	forEachNode(doc, startElement, endElement)
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+	}
+}
