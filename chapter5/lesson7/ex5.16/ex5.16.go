d830053d2715c427f391adb2f4a0f0ac03c689ad
Alex Sysoev <dr.wather@gmail.com>
2023-03-14 20:40:57 +0300

    complete ex5.16

--- chapter5/lesson7/ex5.16/ex5.16.go
+++ chapter5/lesson7/ex5.16/ex5.16.go
@@ -1,11 +1,37 @@
 /*
 Exercise 5.16
-
+Напишите вариативную версию функции strings.Join.
+Write a variadic version of strings.Join.
 */
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	fmt.Println(join("_", "Hello,", "World!"))
+	fmt.Println(join("_", "Hello,"))
+	fmt.Println(join("_"))
 
+	fmt.Println(strings.Join([]string{"Hello,", "World!"}, "_"))
+	fmt.Println(strings.Join([]string{"Hello,"}, "_"))
+	fmt.Println(strings.Join([]string{}, "_"))
+
+}
+
+func join(sep string, args ...string) string {
+	if len(args) == 0 {
+		return ""
+	}
+	var builder strings.Builder
+	for i, s := range args {
+		if i > 0 {
+			builder.WriteString(sep)
+		}
+		builder.WriteString(s)
+	}
+	return builder.String()
 }
