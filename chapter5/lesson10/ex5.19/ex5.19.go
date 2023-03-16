e18f3935fe2cbb2d3f8d2e81c7e0ed8cc40859ef
Alex Sysoev <dr.wather@ya.ru>
2023-03-16 21:00:55 +0300

    complete exercises form lesson10

--- chapter5/lesson10/ex5.19/ex5.19.go
+++ chapter5/lesson10/ex5.19/ex5.19.go
@@ -1,11 +1,27 @@
 /*
 Exercise 5.19
+Воспользуйтесь функциями panic и recover для написания функции,
+которая не содержит инструкцию return, но возвращает ненулевое значение.
 
+Use panic and recover to write a function that contains no return statement yet returns a non-zero value.
 */
 
 package main
 
+import "fmt"
 
 func main() {
+	fmt.Println(returnRecover())
+}
 
+// returnRecover возвращает значение после восстановления из паники
+// без использования `return`
+func returnRecover() (result int, err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			result = 42
+			err = fmt.Errorf("recovered msg: %v", p)
+		}
+	}()
+	panic("It's panic!")
 }
