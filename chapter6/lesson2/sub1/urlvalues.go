829063eb8e4b51b333418767ae146d562979f954
Alex Sysoev <dr.wather@gmail.com>
2023-03-19 17:12:07 +0300

    add notes and example for sub1 lesson2

---
+++ chapter6/lesson2/sub1/urlvalues.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	m := url.Values{"lang": {"en"}} // Непосредственное создание
+	m.Add("item", "1")
+	m.Add("item", "2")
+
+	fmt.Println(m.Get("lang")) // en
+	fmt.Println(m.Get("q"))    // ""
+	fmt.Println(m.Get("item")) // 1 (первое значение)
+	fmt.Println(m["item"])     // [1 2] (непосредственное обращение)
+
+	m = nil
+	fmt.Println(m.Get("item"))  // ""
+	url.Values(nil).Get("item") // "" - эквивалентна предыдущей записи
+	m.Add("item", "3")          // panic: присваивание записи в пустой карте.
+}
