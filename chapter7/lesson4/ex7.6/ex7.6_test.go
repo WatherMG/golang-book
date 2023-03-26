993a3203c9899092d48004866b49696e29556778
Alex Sysoev <dr.wather@gmail.com>
2023-03-26 19:04:00 +0300

    complete ex7.6 for lesson4

--- chapter7/lesson4/ex7.6/ex7.6_test.go
+++ chapter7/lesson4/ex7.6/ex7.6_test.go
@@ -1,1 +1,35 @@
 package tempconv
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+var temp = CelsiusFlag("temp", 20.0, "temperature")
+
+func TestCelsiusFlag(t *testing.T) {
+	var tcs = []struct {
+		args   []string
+		expect string
+	}{
+		{[]string{"testflag", "-temp", "0K"}, "-273.15°C\n"},
+		{[]string{"testflag", "-temp", "0°K"}, "-273.15°C\n"},
+		{[]string{"testflag", "-temp", "0C"}, "0.00°C\n"},
+		{[]string{"testflag", "-temp", "0°C"}, "0.00°C\n"},
+		{[]string{"testflag", "-temp", "32F"}, "0.00°C\n"},
+		{[]string{"testflag", "-temp", "32°F"}, "0.00°C\n"},
+	}
+	for _, tc := range tcs {
+		os.Args = tc.args
+		stdout = new(bytes.Buffer)
+		flag.Parse()
+		fmt.Fprintln(stdout, *temp)
+		actual := stdout.(*bytes.Buffer).String()
+		if actual != tc.expect {
+			t.Errorf("Args: %v, Expects: %v, Actual: %v", tc.args, tc.expect, actual)
+		}
+	}
+}
--- chapter7/lesson4/ex7.6/tempconv.go
+++ chapter7/lesson4/ex7.6/tempconv.go
@@ -4,3 +4,54 @@ Exercise 7.6
 */
 
 package tempconv
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+var stdout io.Writer = os.Stdout
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+// FToC преобразует температуру по Фаренгейту в температуру по Цельсию.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32.0) * 5.0 / 9.0) }
+
+// KToC преобразует температуру по Кельвину в температуру по Цельсию
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// Реализуем метод String для всех типов
+func (c Celsius) String() string { return fmt.Sprintf("%.2f°C", c) }
+
+// *celsiusFlag соответствует интерфейсу flag.Value
+type celsiusFlag struct {
+	Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K", "°K":
+		f.Celsius = KToC(Kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("неверная температура %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
