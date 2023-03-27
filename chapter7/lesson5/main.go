240972c422f5b2bed082310daf1e1285488565a7
Alex Sysoev <dr.wather@gmail.com>
2023-03-27 19:35:11 +0300

    add notes and example for lesson5

--- chapter7/lesson5/main.go
+++ chapter7/lesson5/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
+	var w io.Writer
+	fmt.Println(w)
+	w = os.Stdout
+	fmt.Println(w)
+	w = new(bytes.Buffer)
+	fmt.Println(w)
+	w = nil
+	fmt.Println(w)
 
 }
