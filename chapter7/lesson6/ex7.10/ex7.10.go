55cb5872d46ed420f631c8ad072c2dfce83e4809
Alex Sysoev <dr.wather@gmail.com>
2023-03-29 19:07:08 +0300

    complete ex7.10 for lesson6

--- chapter7/lesson6/ex7.10/ex7.10.go
+++ chapter7/lesson6/ex7.10/ex7.10.go
@@ -1,11 +1,51 @@
 /*
 Exercise 7.10
+Тип sort.Interface можно адаптировать для других применений. Напишите функцию IsPalindrome(s sort.Interface) bool,
+которая сообщает, является ли последовательность s палиндромом (другими словами, что обращение последовательности не изменяет ее).
+Считайте, что элементы с индексами i и j равны, если !s.Less(i, j)&&!s.Less(j, i).
 
+Проблема:
+Столкнулся с некорректной работой программы, было связанно с тем, что:
+Для русских символов нужно использовать тип []rune. Так как при использовании []byte
+русские символы используют несколько байт в UFT-8.
 */
 
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+type word []rune
+
+func (w word) Len() int {
+	return len(w)
+}
+
+func (w word) Less(i, j int) bool {
+	return w[i] < w[j]
+}
+
+func (w word) Swap(i, j int) {
+	w[i], w[j] = w[j], w[i]
+}
+
+func equal(i, j int, s sort.Interface) bool {
+	return !s.Less(i, j) && !s.Less(j, i)
+}
+
+func IsPalindrome(s sort.Interface) bool {
+	max := s.Len() - 1
+	for i := 0; i < s.Len()/2; i++ {
+		if !equal(i, max-i, s) {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-
+	s := word("шалаш")
+	fmt.Println(IsPalindrome(s))
 }
