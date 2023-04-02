bf2de654adecafc77b90d8e8aa8727abbf92a401
Alex Sysoev <dr.wather@gmail.com>
2023-04-02 14:36:40 +0300

    add notes and examples for lesson11

--- chapter7/lesson11/main.go
+++ chapter7/lesson11/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	// Пытаемся открыть файл, который не существует
+	_, err := os.Open("/no/such/file")
+	if err != nil {
+		// С помощью декларации типа проверяем тип ошибки
+		if pe, ok := err.(*os.PathError); ok {
+			// Если тип ошибки == PathError выводим подробное сообщение об ошибке
+			fmt.Printf("Операция: %s\nФайл: %s\nОшибка: %v\n", pe.Op, pe.Path, pe.Err)
+		} else {
+			// Иначе это сообщение
+			fmt.Println("Это другая ошибка:", err)
+		}
+	}
 }
