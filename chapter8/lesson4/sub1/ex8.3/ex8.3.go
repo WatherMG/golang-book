28ba2b1d7cdefde9f3bc2f8d60aa858016c5eef8
Alex Sysoev <dr.wather@gmail.com>
2023-04-14 19:55:26 +0300

    add notes and example for lesson4 sub1

---
+++ chapter8/lesson4/sub1/ex8.3/ex8.3.go
@@ -0,0 +1,1 @@
+package ex8_3
---
+++ chapter8/lesson4/sub1/netcat3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+
+	go func() {
+		io.Copy(os.Stdout, conn) // Примечание: игнорируем ошибки
+		log.Println("done")
+		done <- struct{}{} // Сигнал главной горутине
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // Ожидание завершения фоновой горутины
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
