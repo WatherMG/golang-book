00016ed90ddba2973cff2b8ef906a4be508a2992
Alex Sysoev <dr.wather@gmail.com>
2023-04-20 21:52:10 +0300

    complete ex8.8 for lesson7

--- chapter8/lesson7/ex8.8/ex8.8.go
+++ chapter8/lesson7/ex8.8/ex8.8.go
@@ -5,3 +5,69 @@ Exercise 8.8
 10 секунд.
 */
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+const timeout = 10 * time.Second
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn io.ReadWriteCloser) {
+	wg := &sync.WaitGroup{}
+	defer func() {
+		wg.Wait()
+		conn.Close()
+	}()
+	input := bufio.NewScanner(conn)
+
+	ch := make(chan string)
+
+	go func() {
+		for input.Scan() {
+			ch <- input.Text()
+		}
+	}()
+
+	for {
+		select {
+		case text := <-ch:
+			wg.Add(1)
+			go echo(conn, text, 1*time.Second, wg)
+		case <-time.After(timeout):
+			fmt.Fprintln(conn, "\rYou don't enter message. Exit...")
+			return
+		}
+	}
+}
+
+func echo(c io.Writer, text string, duration time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(text))
+	time.Sleep(duration)
+	fmt.Fprintln(c, "\t", text)
+	time.Sleep(duration)
+	fmt.Fprintln(c, "\t", strings.ToLower(text))
+}
