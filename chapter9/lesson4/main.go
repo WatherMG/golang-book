5ed0168615c594531eecec3259fae53f246e43e1
Alex Sysoev <dr.wather@gmail.com>
2023-04-23 20:02:17 +0300

    add notes and examples for lesson4

--- chapter9/lesson4/main.go
+++ chapter9/lesson4/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var x, y int
+var mu sync.Mutex
+
+func main() {
+	go func() {
+		mu.Lock()
+		x = 1
+		fmt.Print("y:", y, " ")
+		mu.Unlock()
+	}()
+
+	go func() {
+		mu.Lock()
+		y = 1
+		fmt.Print("x:", x, " ")
+		mu.Unlock()
+	}()
+	time.Sleep(1 * time.Nanosecond)
 }
