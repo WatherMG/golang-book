49d030fa489b05e9f290654785044c8695b0f480
Alex Sysoev <dr.wather@gmail.com>
2023-04-28 15:10:08 +0300

    complete ex10.3 for lesson7 sub2

--- chapter10/lesson7/sub2/main.go
+++ chapter10/lesson7/sub2/main.go
@@ -1,5 +1,18 @@
-package main
+/*
+С помощью команды fetch http://gopl.io/chl/hello world?go-get=l выясните, какой
+сервис хранит образцы кода для данной книги. (HTTP-запросы от go get включают
+параметр go-get, так что серверы могут отличаться от указанных в обычном запросе
+браузера.)
+*/
 
-func main() {
+package doc
 
-}
+/*<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="go-import" content="gopl.io git https://github.com/adonovan/gopl.io">
+</head>
+<body>
+</body>
+</html>*/
