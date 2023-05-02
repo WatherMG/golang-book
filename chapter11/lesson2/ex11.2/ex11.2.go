d617c3544620fe710b44c5e3c560cdaa54cd14b8
Alex Sysoev <dr.wather@gmail.com>
2023-05-02 20:40:54 +0300

    complete ex11.2 for lesson2

--- chapter11/lesson2/ex11.2/ex11.2.go
+++ chapter11/lesson2/ex11.2/ex11.2.go
@@ -1,11 +1,70 @@
 /*
 Exercise 11.2
+Напишите набор тестов для множества IntSet (раздел 6.5), который
+проверяет, эквивалентно ли его поведение множеству на основе встроенных
+отображений. Сохраните свою реализацию для проверок производительности в
+упражнении 11.7.
 
+Example 6.4
+Пакет intset предоставляет множество целых чисел на основе битового вектора
 */
 
-package main
+package intset
 
+import (
+	"bytes"
+	"fmt"
+)
 
-func main() {
+// IntSet представляет собой множество небольших неотрицательных
+// целых чисел. Нулевое значение представляет пустое множество.
+type IntSet struct {
+	words []uint64
+}
 
+// Has указывает, содержит ли множество неотрицательное значение x
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add добавляет неотрицательное значение x в множество
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith делает  множество s равным объединению множеств s и t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String возвращает множество как строку вида "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
