93289222d1a66ae1a9424b14c8d112e70355067f
Alex Sysoev <dr.wather@gmail.com>
2023-05-04 19:41:00 +0300

    add notes and example for lesson2 sub5

--- chapter11/lesson2/sub5/main.go
+++ chapter11/lesson2/sub5/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
+// Плохая функция
+func assertEqual(x, y int) {
+	if x != y {
+		panic(fmt.Sprintf("%d != %d", x, y))
+	}
+}
+
+func TestSplit(t *testing.T) {
+	words := strings.Split("a:b:c", ":")
+	assertEqual(len(words), 3)
+	// ...
+}
+
+func TestSplitv2(t *testing.T) {
+	s, sep := "a:b:c", ":"
+	words := strings.Split(s, sep)
+	if got, want := len(words), 3; got != want {
+		t.Errorf("Split(%q, %q) возвращает %d слов, а требуется %d", s, sep, got, want)
+	}
 }
