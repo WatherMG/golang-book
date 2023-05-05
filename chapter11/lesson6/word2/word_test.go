adcd5ef9bc344a5e039376fe755489abe4ea04e5
Alex Sysoev <dr.wather@gmail.com>
2023-05-05 17:30:34 +0300

    add notes for lesson6

---
+++ chapter11/lesson6/word2/word_test.go
@@ -0,0 +1,75 @@
+package word
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // Не палиндром
+		{"desserts", false},   // Полупалиндром
+	}
+
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+		}
+	}
+}
+
+// randomPalindrome возвращает палиндром, длина и содержимое
+// которого задаются генератором псевдослучайных чисел rng.
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // Случайная длина до 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // Случайная руна до `\u0999`
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindrome(t *testing.T) {
+	// Инициализация генератора псевдослучайных чисел.
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("ГПСЧ инициализирован: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("RandomPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
+
+func ExampleIsPalindrome() {
+	fmt.Println(IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(IsPalindrome("palindrome"))
+	// Output:
+	// true
+	// false
+}
